d560c42ed340defeb7f0fef20e2f48d953f38779
Darkosz1012 <darek10121998db@gmail.com>
2024-09-29 09:08:24 +0200

    fix: removes console log

--- src/osrm-api.ts
+++ src/osrm-api.ts
@@ -15,7 +15,7 @@ export async function fetchRoutes(options: FetchRouteOptions) {
   try {
     const result = await fetch(
       `${ROUTE_API}/v1/driving/${coordinatesStr}?overview=false&alternatives=true&steps=true&geometries=geojson&annotations=nodes`,
-    ).then((r) =>{ console.log(r); return r.json();});
+    ).then((r) =>{ return r.json();});
  
     return RouteResponse.parse(result);
 
